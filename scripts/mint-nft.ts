7bf271b664d24d00452c1354141219a6910cb602
toshiki tomihira <toshiki19970714@gmail.com>
2022-01-16 19:08:19 +0900

    Add nft storage

--- scripts/mint-nft.ts
+++ scripts/mint-nft.ts
@@ -5,15 +5,18 @@
 // Runtime Environment's members available in the global scope.
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 import { exit } from "process";
+import { NFTStorage, File } from "nft.storage";
+import path from "path";
+import fs from "fs";
 
 require("dotenv").config();
-const API_URL = process.env.ROPSTEN_URL;
-const PUBLIC_KEY = process.env.PUBLIC_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_URL = process.env.ROPSTEN_URL!;
+const PUBLIC_KEY = process.env.PUBLIC_KEY!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const NFTSTORAGE_API_KEY = process.env.NFTSTORAGE_API_KEY!;
 
-if (!API_URL || !PUBLIC_KEY || !PRIVATE_KEY) {
+if (!API_URL || !PUBLIC_KEY || !PRIVATE_KEY || !NFTSTORAGE_API_KEY) {
   console.error("env is not set");
-  process.exitCode = 1;
   exit();
 }
 
@@ -21,14 +24,25 @@ const web3 = createAlchemyWeb3(API_URL);
 const contract = require("../artifacts/contracts/PokumonNFT.sol/PokumonNFT.json");
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // Input NFT contract address
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+const storageClient = new NFTStorage({ token: "API_TOKEN" });
 
-async function mintNFT(
-  tokenURI: string,
-  name: string,
-  publicKey: string,
-  privateKey: string
-) {
-  const nonce = await web3.eth.getTransactionCount(publicKey, "latest"); // get latest nonce
+async function uploadIPFS(filePath: string, name: string, description: string) {
+  const imageData = await fs.readFileSync(filePath, "binary");
+  const fileName = path.basename(filePath);
+  const metadata = await storageClient.store({
+    name: name,
+    description: description,
+    image: new File([imageData], fileName, {
+      type: "image/jpg",
+    }),
+  });
+  console.log(metadata.url);
+  return metadata.url;
+  // ipfs://bafyreib4pff766vhpbxbhjbqqnsh5emeznvujayjj4z2iu533cprgbz23m/metadata.json
+}
+
+async function mintNFT(tokenURI: string, name: string) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); // get latest nonce
 
   // the transaction
   const tx = {
@@ -41,16 +55,22 @@ async function mintNFT(
       .encodeABI(),
   };
 
-  const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
+  const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
   const result = await web3.eth.sendSignedTransaction(signedTx.rawTransaction!);
   console.log(result.transactionHash);
   return result.transactionHash;
 }
 
-module.exports = mintNFT;
-mintNFT(
-  "https://gateway.pinata.cloud/ipfs/QmNakDL3RXm2Q8AzVU8CX1KKHQvBE6VtzFsoN5oGDkUZz6",
-  "Pikachu",
-  PUBLIC_KEY,
-  PRIVATE_KEY
-);
+async function main() {
+  const namesFile = fs.readFileSync("../ai_models/pokumon_names.txt", "utf8");
+  const names = namesFile.toString().split("\n");
+  for (let i = 0; i < 10; i++) {
+    const name = names[i];
+    const description = "";
+    const filePath = "../ai_models/images/" + i + ".jpg";
+    const tokenURI = await uploadIPFS(filePath, name, description);
+    mintNFT(tokenURI, name);
+  }
+}
+
+main();
